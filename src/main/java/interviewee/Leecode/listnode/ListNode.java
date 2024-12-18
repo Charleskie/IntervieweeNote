fb4895c9aecb4d1e18e670c98c0c914bc86051fc
王胜 <shengwang@shopee.com>
2024-12-18 17:26:27 +0800

    算法复习

--- src/main/java/interviewee/Leecode/listnode/ListNode.java
+++ src/main/java/interviewee/Leecode/listnode/ListNode.java
@@ -3,9 +3,11 @@ package interviewee.Leecode.listnode;
 // Definition for singly-linked list.
 public class ListNode {
     public int val;
+    public ListNode pre;
     public ListNode next;
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int val, ListNode pre, ListNode next) { this.val = val; this.pre = pre; this.next = next; }
 }
 
